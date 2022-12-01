a4f69a52c3de5c12b2b7602909aafc20ebc56f56
mplaczek99 <mplaczek99@icloud.com>
2022-11-30 20:56:49 -0600

    For Loops

--- myscript.sh
+++ myscript.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
 
-release_file=/etc/os-release
-
-if grep -q "Arch" $release_file
-then
-    # The host is based on Arch, run the pacman update command
-    sudo pacman -Syu
-fi
-
-if grep -q "Pop" $release_file || grep -q "Ubuntu" $release_file
-then
-    # The host is based on Ubuntu
-    # Run the apt version of the command
-    sudo apt update
-    sudo apt dist-upgrade
-fi
+for file in logfiles/*.log
+do
+    tar -czvf $file.tar.gz $file
+    rm $file
+done
