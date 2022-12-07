2b5a573b574a58de85545595933d786513fd340f
mplaczek99 <mplaczek99@icloud.com>
2022-12-06 22:48:48 -0600

    Scheduling Jobs (Part 1)

--- myscript.sh
+++ myscript.sh
@@ -1,31 +1,5 @@
 #!/bin/bash
 
-finished=0
+logfile=job_results.log
 
-while [ $finished -ne 1 ]
-do
-    echo "What is your favorite Linux distribution?"
-
-    echo "1 - Arch"
-    echo "2 - CentOS"
-    echo "3 - Debian"
-    echo "4 - Mint"
-    echo "5 - Ubuntu"
-    echo "6 - Something else..."
-    echo "7 - Exit the script."
-
-    read distro;
-
-    case $distro in
-        1) echo "Arch is a rolling release.";;
-        2) echo "CentOS is popular on servers.";;
-        3) echo "Debian is a community distribution.";;
-        4) echo "Mint is popular on desktops and laptops.";;
-        5) echo "Ubuntu is popular on both servers and computers.";;
-        6) echo "There are many distributions out there.";;
-        7) finished=1 ;;
-        *) echo "you didn't enter an appropriate choice."
-    esac
-done
-
-echo "Thank you for using this script."
+echo "The script ran at the following time: $(date)" > $logfile
